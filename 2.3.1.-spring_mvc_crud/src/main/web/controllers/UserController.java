b5adbe8a2b9537ab5f90beb3c3c84959711c1e28
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-03-31 17:56:38 +0300

    Commit 4 31.03.21 Change UserController and users.html.

--- 2.3.1.-spring_mvc_crud/src/main/web/controllers/UserController.java
+++ 2.3.1.-spring_mvc_crud/src/main/web/controllers/UserController.java
@@ -1,4 +1,13 @@
 package web.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+
+    @GetMapping()
+    public String getAllUser() {
+        return "users";
+    }
 }
