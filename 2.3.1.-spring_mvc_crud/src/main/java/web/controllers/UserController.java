1c4b41a31984a745856f050c82f3c734892274e7
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-04-05 19:38:51 +0300

    Commit 2 05.04.21 Add user_create.html, change UserController, user_list.html.

--- 2.3.1.-spring_mvc_crud/src/main/java/web/controllers/UserController.java
+++ 2.3.1.-spring_mvc_crud/src/main/java/web/controllers/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import web.dao.UserDao;
 import web.model.User;
 
@@ -28,8 +29,18 @@ public class UserController {
     @GetMapping("/users")
     public String getAllUser(Model model) {
         List<User> allUser = userDao.getAllUser();
-        //List<User> allUser = users;
         model.addAttribute("allUser", allUser);
         return "users_list";
+    }
+
+    @GetMapping("/user_create")
+    public String createUserAndShow() {
+        return "user_create";
+    }
+
+    @PostMapping("/user_create")
+    public String createUser(User user) {
+        userDao.saveUser(user);
+        return "redirect:/users_list";
     }
 }
